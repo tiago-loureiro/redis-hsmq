63b37b80b4ed1f7adc10dbe2fce7d4b28aa30a19
Matthias Fischmann <mf@zerobuzz.net>
2018-06-08 18:01:58 +0200

    ...

--- src/Main.hs
+++ src/Main.hs
@@ -2,5 +2,15 @@ module Main where
 
 import Database.Redis.IO
 
-lpush
-brpoplpush
+
+-- https://redis.io/commands/lpush
+lpush :: _
+
+-- https://redis.io/commands/rpoplpush
+brpoplpush :: _
+
+
+
+-- https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_CreateQueue.html
+-- https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_SendMessage.html
+-- https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html
