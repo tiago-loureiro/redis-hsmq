441dee699cf59a558ff699810cc1fb2ff0047928
Damian Nadales <damian.nadales@gmail.com>
2018-06-10 15:17:28 +0200

    Use `request` instead of `post` for sending JSON.

--- src/RedisHSMQ/Server.hs
+++ src/RedisHSMQ/Server.hs
@@ -1,27 +1,25 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE TypeOperators #-}
 
 module RedisHSMQ.Server where
 
-import           Control.Monad              (void)
-import           Control.Monad.Trans.Reader (ReaderT, ask, runReaderT)
-import           Data.Text                  (Text)
-import           Database.Redis.IO
-import           Network.Wai.Handler.Warp   (run)
-import           Servant
+import Control.Monad (void)
+import Control.Monad.Trans.Reader (ReaderT, ask, runReaderT)
+import Data.Text (Text)
+import Database.Redis.IO
+import Network.Wai.Handler.Warp (run)
+import Servant
 
-import qualified RedisHSMQ.IO               as RIO
-import qualified RedisHSMQ.Types            as RT
-import qualified System.Logger              as Logger
+import qualified RedisHSMQ.Types as RT
+import qualified RedisHSMQ.IO    as RIO
+import qualified System.Logger   as Logger
 
 type GetMessage = Get '[JSON] RT.Message
 type AddMessage = ReqBody '[JSON] RT.Message :> PostCreated '[JSON] ()
 type RedisHSMQAPI = (Capture "accountnr" Text :> Capture "queue" RT.QueueName :> GetMessage) :<|>
-                    (Capture "accountnr" Text :> Capture "queue" RT.QueueName :> AddMessage) :<|>
-                    DummyEP
-type DummyEP = "dummy" :>  Capture "queue" String  :> Post '[JSON] ()
+                    (Capture "accountnr" Text :> Capture "queue" RT.QueueName :> AddMessage)
 
 api :: Proxy RedisHSMQAPI
 api = Proxy
@@ -34,9 +32,9 @@ type AppM = ReaderT State Handler
 
 mkEnv :: IO State
 mkEnv = do
-    -- g <- Logger.new Logger.defSettings
-    -- p <- mkPool g (setHost "localhost" defSettings)
-    return $ State undefined -- p
+    g <- Logger.new Logger.defSettings
+    p <- mkPool g (setHost "localhost" defSettings)
+    return $ State p
 
 startServer :: State -> IO ()
 startServer st = run 8080 $ app $ st
@@ -45,7 +43,7 @@ stopServer :: State -> IO ()
 stopServer st = shutdown (redis st)
 
 server :: ServerT RedisHSMQAPI AppM
-server = getMessage :<|> addMessage :<|> dummy
+server = getMessage :<|> addMessage
   where
     getMessage _ qn = do
       State p <- ask
@@ -59,8 +57,6 @@ server = getMessage :<|> addMessage :<|> dummy
       void . runRedis p $ commands
                         $ RIO.enqueue qn
                         $ msg
-
-    dummy _ = return ()
 
 app :: State -> Application
 app s = serve api $ hoistServer api (flip runReaderT s) server
--- test/RedisHSMQ/ServerSpec.hs
+++ test/RedisHSMQ/ServerSpec.hs
@@ -2,23 +2,25 @@
 -- | End-to-end tests for the server.
 module RedisHSMQ.ServerSpec where
 
-import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson             (encode)
-import           Data.UUID.V4           (nextRandom)
-import           Test.Hspec             (Spec, describe, it)
-import           Test.Hspec.Wai         (get, post, shouldRespondWith, with)
+import           Control.Monad.IO.Class    (liftIO)
+import           Data.Aeson                (encode)
+import           Data.UUID.V4              (nextRandom)
+import           Network.HTTP.Types.Method (methodPost)
+import           Test.Hspec                (Spec, describe, it)
+import           Test.Hspec.Wai            (get, post, request,
+                                            shouldRespondWith, with)
 
-import           RedisHSMQ.Server       (app, mkEnv)
-import           RedisHSMQ.Types        (Message (Message),
-                                         VisibilityTimeout (VisibilityTimeout))
+import           RedisHSMQ.Server          (app, mkEnv)
+import           RedisHSMQ.Types           (Message (Message), VisibilityTimeout (VisibilityTimeout))
 
 spec :: Spec
 spec = with (app <$> mkEnv) $ do
     describe "Basic sending and receiving" $
         it "puts a message and then gets it" $ do
             uuid <- liftIO nextRandom
-            let msg = encode $ Message "hello" uuid vTimeout
+            let
+                msg = encode $ Message "hello" uuid vTimeout
                 vTimeout = VisibilityTimeout 1000
-            post "/dummy/hello/" "" `shouldRespondWith` 201
+            request methodPost "/dummy/hello/world"  [("Content-Type", "application/json")] msg `shouldRespondWith` 201
             -- post "/theaccount/thequeue" msg `shouldRespondWith` 201
 
